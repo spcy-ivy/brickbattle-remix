93fb5c9a826b5f01150e4b79c6c5eab375c49f64
WorshipMeOrElse <sylviaviajung@gmail.com>
2024-02-10 17:22:14 -0800

    fix: changed docusaurus baseUrl

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://spcy-ivy.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/enhanced-brickbattle/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.

64cd195721c39f7bd40680d3eb2c0c4f2dbeac72
WorshipMeOrElse <sylviaviajung@gmail.com>
2024-02-10 16:41:06 -0800

    feat: PLEASE WORK ACTIONS PLEASE

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -13,14 +13,14 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://spcy-ivy.github.io/enhanced-brickbattle ',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'spcy', // Usually your GitHub org/user name.
+  organizationName: 'spcy-ivy', // Usually your GitHub org/user name.
   projectName: 'enhanced-brickbattle', // Usually your repo name.
 
   onBrokenLinks: 'throw',

5a95a53d7c1ce09041fd4fe4f1b266da02ca4e8c
WorshipMeOrElse <sylviaviajung@gmail.com>
2024-02-22 09:11:37 -0800

    feat: mobile controls now work

--- src/client/ui/mobileControls.tsx
+++ src/client/ui/mobileControls.tsx
@@ -1,14 +1,24 @@
 import React from "@rbxts/react"
 import { useRem } from "./useRem";
+import { actionSignal } from "client/signals";
 
 interface ButtonProps {
   size?: number,
   offset?: Vector2,
-  imageId?: string
-  color?: Color3
+  imageId?: string,
+  color?: Color3,
+  pressedEvent?: () => void
 }
 
-export function Button({ size = 10, offset = Vector2.one, imageId = "rbxassetid://7220505716", color = Color3.fromHex("#FFFFFF") }: ButtonProps) {
+export function Button({
+  size = 10,
+  offset = Vector2.one,
+  imageId = "rbxassetid://7220505716",
+  color = Color3.fromHex("#FFFFFF"),
+  pressedEvent = () => {
+    print("you didnt bind this to anything yet...")
+  }
+}: ButtonProps) {
   const rem = useRem();
 
   return (
@@ -17,6 +27,9 @@ export function Button({ size = 10, offset = Vector2.one, imageId = "rbxassetid:
       Size={UDim2.fromOffset(rem(size), rem(size))}
       Image={imageId}
       BackgroundColor3={color}
+      Event={{
+        MouseButton1Click: pressedEvent
+      }}
     >
       <uicorner CornerRadius={new UDim(1)} />
     </imagebutton>
@@ -26,9 +39,24 @@ export function Button({ size = 10, offset = Vector2.one, imageId = "rbxassetid:
 export function MobileControls() {
   return (
     <>
-      <Button key="primary-button" />
-      <Button key="secondary-button" size={6} offset={new Vector2(3, 12)} />
-      <Button key="tertiary-button" size={6} offset={new Vector2(10, 9)} />
+      <Button
+        key="jump-button"
+        pressedEvent={() => { actionSignal.fire("Jump") }}
+      />
+
+      <Button
+        key="airdash-button"
+        size={6}
+        offset={new Vector2(3, 12)}
+        pressedEvent={() => { actionSignal.fire("Airdash") }}
+      />
+
+      <Button
+        key="fastfall-button"
+        size={6}
+        offset={new Vector2(10, 9)}
+        pressedEvent={() => { actionSignal.fire("FastFall") }}
+      />
     </>
   )
 }

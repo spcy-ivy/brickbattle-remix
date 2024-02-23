a88a71b1ac89f8f457a57fdd090401f62eb22497
WorshipMeOrElse <sylviaviajung@gmail.com>
2024-02-22 16:30:16 -0800

    fix: camera lock offset now

--- src/client/cameralock.client.ts
+++ src/client/cameralock.client.ts
@@ -1,4 +1,4 @@
-import { Players, RunService, UserInputService, Workspace } from "@rbxts/services";
+import { Players, RunService, StarterPlayer, UserInputService, Workspace } from "@rbxts/services";
 import { Cleanup } from "shared/cleanup";
 import { CharacterRigR6 } from "types/characterRigR6";
 
@@ -6,10 +6,14 @@ const renderStepName = "CameraLock";
 const cleanup = Cleanup();
 const camera = Workspace.CurrentCamera as Camera;
 
+StarterPlayer.EnableMouseLockOption = false;
+
 Players.LocalPlayer.CharacterAppearanceLoaded.Connect((model) => {
 	const character = model as CharacterRigR6;
 	const rootpart = character.HumanoidRootPart;
 	const humanoid = character.Humanoid;
+
+	humanoid.CameraOffset = new Vector3(1.75, 0, 0);
 
 	RunService.BindToRenderStep(renderStepName, Enum.RenderPriority.Character.Value, () => {
 		UserInputService.MouseBehavior = Enum.MouseBehavior.LockCenter;
